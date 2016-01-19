fecf6ee76aecc2134f752722a3485b726bd323e1
Lalourche <la_lourche@yahoo.fr>
2016-01-19 15:43:51 +0100

    Introducing TestFX in dependencies.

---
+++ src/test/java/HelloJavaFxTest.java
@@ -0,0 +1,13 @@
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.stage.Stage;
+
+public class HelloJavaFxTest extends ApplicationTest {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
